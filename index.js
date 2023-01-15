045ab0a8b397eeaa38d03416763360e113a3eefa
Amiri Hayes <89553495+AmiriHayes@users.noreply.github.com>
2023-01-15 17:42:46 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //Code for the hamburger menu
-
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 
@@ -14,44 +13,74 @@ navLinks.forEach(link => {
 })
 
 //Code for the login page
-
 const loginButton = document.getElementById("loginButton")
 const loginErrorMsg = document.getElementById("loginErrorMsg")
 
-//Fill in values for the table on homepage; As of 9/1/22 the table is
+//Fill in values for the table on homepage; As of 12/1/23 the table is
 //10 across by 20 down. Top and bottom elements are header and foot.
+
+//Manipulates Table Elements based on screen-size
+document.addEventListener('DOMContentLoaded', init);
+function init() {
+    let query_one = window.matchMedia("(max-width: 800px)");
+    let query_two = window.matchMedia("(max-width: 500px)");
+    if (query_one.matches) {
+        for (var i = 0; i < myTable.rows.length; i++) {
+            myTable.rows[i].deleteCell(1); //delete date column
+            myTable.rows[i].deleteCell(8); //delete %increase
+            myTable.rows[i].deleteCell(7); //delete %portfolio
+            myTable.rows[i].deleteCell(6); //delete %portfolio
+        }
+    }
+    if (query_two.matches) {
+        for (var i = 0; i < myTable.rows.length; i++) {
+            myTable.rows[i].deleteCell(1); //delete # of stocks
+            myTable.rows[i].deleteCell(1); //delete price
+            myTable.rows[i].deleteCell(2); //delete current price
+        }
+    }
+}
+
+/*Gets the stock data from CSV file which constantly updates (YOUTUBE)
+const fs = require("fs");
+const csv = require('csvtojson');
+(async () => {
+    var price_data = await csv().fromFile("datasheet.csv");
+    console.log(price_data);
+})();*/
+
+/*Gets the stock data from CSV file which constantly updates (GEEKSFORGEEKS)
+const fs = require("fs");
+csv = fs.readFileSync("datasheet.csv");
+var price_data = csv.toString().split("\r");*/
 
 var myTable = document.getElementById('table');
 
-//Current Price Column]
-//src="jquery-csv.js"
-//var csv = require('jquery-csv');
-//var currentPrice = $.csv.toArrays(datasheet.csv);
+//Initializes currentPrice array using Python CSV data
+var currentPrice = [117.01,134.76,99.4,134.76,99.4,43.5,43.5,49.92,49.92,112.15,113.51,113.51,239.23,239.23,69.49,94.96,120.31,120.31];
+for (var k = 1; k < (myTable.rows.length-1); k++) {
+    myTable.rows[k].cells[5].innerHTML = '$' + currentPrice[k-1].toFixed(2);
+}
 
-//currentPrice = [];
-originalPrice = [];
+var originalPrice = [];
 for (var j = 1; j < (myTable.rows.length-1); j++) {
     originalPrice[j] = myTable.rows[j].cells[3].innerHTML;
 }
-for (var j = 1; j < (myTable.rows.length-1); j++) {
-    currentPrice[j] = '0.00';
-    myTable.rows[j].cells[5].innerHTML = '$' + currentPrice[j];
-}
 
 //Current Value Column
-currentValue = [];
-shares = [];
+var currentValue = [];
+var shares = [];
 for (var j = 1; j < (myTable.rows.length-1); j++) {
-    shares[j] = myTable.rows[j].cells[2].innerHTML;
+    shares[j-1] = myTable.rows[j].cells[2].innerHTML;
 }
-for (var j = 1; j < (myTable.rows.length-1); j++) {
-    currentValue[j] = currentPrice[j] * shares[j];
-    myTable.rows[j].cells[6].innerHTML = '$' + currentValue[j].toFixed(2);
+for (var m = 1; m < (myTable.rows.length-1); m++) {
+    currentValue[m] = currentPrice[m-1] * shares[m-1];
+    myTable.rows[m].cells[6].innerHTML = '$' + currentValue[m].toFixed(2);
 }
 
 //Current Gain Column
-currentGain = [];
-originalCost = [];
+var currentGain = [];
+var originalCost = [];
 for (var i = 1; i < (myTable.rows.length-1); i++) {
     originalCost[i] = dollarToNum(myTable.rows[i].cells[4].innerHTML);
 }
@@ -65,7 +94,7 @@ for (var i = 1; i < (myTable.rows.length-1); i++) {
 }
 
 //%Increase Column
-pIncrease = [];
+var pIncrease = [];
 for (var i = 1; i < (myTable.rows.length-1); i++) {
     pIncrease[i] = ((currentValue[i] - originalCost[i])/(originalCost[i])*100);
     if (pIncrease[i] == -100) {
@@ -100,22 +129,6 @@ for (var i = 1; i < (myTable.rows.length-1); i++) {
         myTable.rows[i].cells[9].innerHTML = '0%';
     } else {
         myTable.rows[i].cells[9].innerHTML = pPortfolio[i].toFixed(2) + '%';
-    }
-}
-
-//Manipulates Table Elements based on screen-size
-document.addEventListener('DOMContentLoaded', init);
-
-function init() {
-    let query = window.matchMedia("(max-width: 800px)");
-    if (query.matches) {
-
-        for (var i = 0; i < myTable.rows.length; i++) {
-            myTable.rows[i].deleteCell(1); //delete date column
-            myTable.rows[i].deleteCell(8); //delete %increase
-            myTable.rows[i].deleteCell(7); //delete %portfolio
-            myTable.rows[i].deleteCell(6); //delete %portfolio
-        }
     }
 }
 
